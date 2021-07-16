3f20d7158178a9348ac81ddd94b20a0cbca669b3
unknown <houssemhosni2018@gmail.com>
2021-07-16 11:35:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Form>
+        <Form.Group controlId="form.Name">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" placeholder="Enter name" />
+        </Form.Group>
+        <Form.Group controlId="form.Email">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group controlId="form.Textarea">
+            <Form.Label>Comment</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </Form>
+    </Container>
   );
 }
 
